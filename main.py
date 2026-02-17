67f0722b891a10feea06288c4cf4059415a3c572
mlagg437-art <mlagg437@bjalearn.net>
2026-02-17 14:31:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,40 +6,40 @@
 def calculate_total_price(products):
     total = 0
     for item in products:
-        total = total + item["price"]
+        total = total + float(item["price"])
     return total
 
-
 def apply_discount(price, discount):
+    price = float(price)
+    discount = float(discount)
     if discount > 1:
         discount = discount / 100
     return price - price * discount
 
-
 def update_stock(products, product_name, amount):
+    amount = int(amount)
+    updated_product = None
     for product in products:
         if product["name"] == product_name:
-            product["stock"] = product["stock"] - amount
-    return product
-
+            product["stock"] = int(product["stock"]) - amount
+            updated_product = product
+        break
+    return updated_product
 
 def get_average_price(products):
     total = calculate_total_price(products)
-    return total / products
-
+    return total / len(products)
 
 def print_products(products):
     for i in range(len(products)):
-        print(products["name"], "-", products[i]["price"], "-", products[i]["stock"])
-
+        print(products[i]["name"], "-", products[i]["price"], "-", products[i]["stock"])
 
 products = [
-    {"name": "Pencil", "price": 0.99, "stock": 100},
-    {"name": "Notebook", "price": "2.50", "stock": 50},
-    {"name": "Backpack", "price": 25, "stock": "20"},
-    {"name": "Marker", "price": 1.5}
+{"name": "Pencil", "price": 0.99, "stock": 100},
+{"name": "Notebook", "price": 2.50, "stock": 50},
+{"name": "Backpack", "price": 25, "stock": 20},
+{"name": "Marker", "price": 1.5, "stock": 0}
 ]
-
 
 print("Product list:")
 print_products(products)
@@ -60,21 +60,21 @@ print("Average price:", average)
 
 
 choice = input("Enter product name to buy: ")
-quantity = input("Enter quantity: ")
+quantity = int(input("Enter quantity: "))
 
 for product in products:
     if product["name"] == choice:
         if quantity <= product["stock"]:
             product["stock"] = product["stock"] - quantity
-            print("Purchase successful")
-        else
-            print("Not enough stock")
+    print("Purchase successful")
+else:
+    print("Not enough stock")
 
 
 count = 0
 while count < len(products):
     print(products[count]["name"])
-    count = count - 1
+    count = count + 1
 
 
 print("Done")
